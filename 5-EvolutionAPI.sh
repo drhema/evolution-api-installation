02af6a0bf4ccbcceeb3347beb43fb43f4ff91593
Ibrahim Elsherbiny <dr_ibrahimm@yahoo.com>
2024-03-08 16:15:44 +0300

    Update 5-EvolutionAPI.sh

--- 5-EvolutionAPI.sh
+++ 5-EvolutionAPI.sh
@@ -17,7 +17,12 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-
+sudo ufw allow 8080/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw allow 3000/tcp
+sudo ufw allow 6379/tcp
+sudo ufw allow 5432/tcp
 # Wait 5 seconds
 echo "Waiting for 5 seconds..."
 sleep 5
