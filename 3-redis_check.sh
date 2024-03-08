df01b15f2ec806fc408ca7b6887b70c24f62acc1
Ibrahim Elsherbiny <dr_ibrahimm@yahoo.com>
2024-03-08 13:33:07 +0300

    Update 3-redis_check.sh

--- 3-redis_check.sh
+++ 3-redis_check.sh
@@ -2,15 +2,18 @@
 
 # Automatically determine the Redis server IP address
 REDIS_SERVER_IP=$(hostname -I | awk '{print $1}')
-REDIS_PASSWORD="your_redis_password" # Leave this empty if no password is set: REDIS_PASSWORD=""
+
+# Prompt the user for the Redis password
+echo -n "Enter Redis password: "
+read -s REDIS_PASSWORD
+echo
 
 # Function to test Redis connectivity and operations
 test_redis() {
     echo "Trying to connect to Redis server at $REDIS_SERVER_IP..."
 
-    # If a password is required, use the -a option to authenticate
+    # Check connection with authentication if a password was provided
     if [ -n "$REDIS_PASSWORD" ]; then
-        # Check connection with authentication
         response=$(echo -e "AUTH $REDIS_PASSWORD\r\nSET testkey 'Hello, Redis!'\r\nGET testkey\r\nQUIT" | nc $REDIS_SERVER_IP 6379)
     else
         # Check connection without authentication
